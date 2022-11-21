f997ad75fcba9c49dd05c00179f102aeaf50397c
yang9098 <105197533+yang9098@users.noreply.github.com>
2022-11-21 17:25:30 +0900

    Update dy1201.java

--- dy1201.java
+++ dy1201.java
@@ -1,3 +1,5 @@
+//커피숍
+
 package 커피숍;
 
 public class 음료 {
@@ -37,3 +39,39 @@ public class 아이스티 extends 음료{
 	
 
 }
+//다형성
+package 다형성;
+
+public class PersonTest {
+	
+	public static void main(String []args) {
+		
+		Person myPerson = new Person();
+		myPerson.name = "홍길동";
+		myPerson.number = 1111;
+		myPerson.info();
+		
+		Person yourPerson = new Person();
+		yourPerson.name = "이순신";
+		yourPerson.number = 2222;
+		yourPerson.info();
+		
+		Person otherPerson = myPerson;
+		otherPerson.info();
+		
+	}
+}
+
+package 다형성;
+
+public class Person {
+	String name;
+	int number;
+	
+	public void info() {
+		System.out.println("name: " + name);
+		System.out.println("number: " + number);
+	}
+}
+
+
