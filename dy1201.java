5d7ac807237a5a23fc425d01504496be3e15dc4f
yang9098 <105197533+yang9098@users.noreply.github.com>
2022-11-21 15:50:32 +0900

    Create dy1201.java

---
+++ dy1201.java
@@ -0,0 +1,39 @@
+package 커피숍;
+
+public class 음료 {
+	
+	int 사이즈;
+	int 가격;
+	boolean 아이스여부;
+	
+}
+
+
+package 커피숍;
+
+public class 라떼 extends 커피 {
+
+}
+
+
+package 커피숍;
+
+public class 아메리카노 extends 커피 {
+
+}
+
+
+package 커피숍;
+
+public class 커피 extends 음료{
+	
+	String 원두;
+}
+
+
+package 커피숍;
+
+public class 아이스티 extends 음료{
+	
+
+}
